41378fc8491f85bf02a01f2b05fdc3306c2ee3a2
Seth Harden <sethmh82@users.noreply.github.com>
2017-06-18 20:47:01 -0700

    added twitter

--- liri.js
+++ liri.js
@@ -2,3 +2,20 @@
 var keys = require("./keys.js");
 var Twitter = require("twitter");
 var spotify = require("spotify");
+
+
+// TWITTER
+var showTweets = function() { 
+  var tKeys = new Twitter(keys.twitterKeys);
+  client.get("statuses/user_timeline", "statisticbrain", function(err, tweet, res) {
+      for (var i = 0; i < tweet.length; i++) {
+          console.log(tweet[i].text);
+      }
+   });
+};
+
+//MY API KEY 32b70d0e
+var showMovie = function(theMovie) {
+  var obdb = "http://www.omdbapi.com/?t=" + theMovie + "&y=&plot=full&tomatoes=true&apikey=32b70d0e";
+  
+};
